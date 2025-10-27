a5c35fbd407091a39e58c5dc4044f7b983dc8a26
kballadi <kalyan.alladi@neudesic.com>
2025-10-27 21:33:00 +0530

    Added discount price cal file

---
+++ fundamentals/discount-price.py
@@ -0,0 +1,6 @@
+original_price = 75
+discount_percentage = 15
+
+discount = original_price * (discount_percentage / 100)
+final_price = original_price - discount
+print("The final price after discount is:", final_price)
